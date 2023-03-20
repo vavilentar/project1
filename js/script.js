e83be68958377edec1f67cbb791e6db73fdcc968
Pchelkin Roman <i@rpchelkin.ru>
2023-03-20 15:36:18 +0300

    update: add mailer

--- js/script.js
+++ js/script.js
@@ -1,10 +1,34 @@
 const feedbackBtns = document.querySelectorAll('.feedback-btn');
+const feedbackModal = document.querySelector('.feedback-modal');
+const feedbackCloseBtn = document.querySelector('.close-modal');
 
 feedbackBtns.forEach(item => {
 	item.addEventListener('click', (e) => {
-		console.log(e.target)
+		feedbackModal.classList.remove('feedback-modal_closed')
 	})
 })
+
+feedbackCloseBtn.addEventListener('click', (e) => {
+	feedbackModal.classList.add('feedback-modal_closed')
+})
+
+$('#phone').mask("+7 (999) 999-9999");
+
+$('form').submit(function(e) {
+	e.preventDefault();
+	$.ajax({
+		type: "POST",
+		url: "../mailer/smart.php",
+		data: $(this).serialize()
+	}).done(function() {
+		$(this).find("input").val("");
+
+
+		$('form').trigger('reset');
+	});
+
+	return false;
+});
 
 
 var slider = tns({
